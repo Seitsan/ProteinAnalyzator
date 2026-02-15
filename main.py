5407ddae735c051d27e9c76bcd40ab4f9801e164
Seitsan <den.kolodin.06@gmail.com>
2026-02-15 13:48:56 +0300

    Draft CLI realisation

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 from src.analyzer import ProteinAnalyzer
 from pathlib import Path
+import click
+
+@click.group()
+def cli():
+    pass
 
 
-fasta_file = Path("insulin.fasta")
-output_file = Path("stats.txt")
-protan = ProteinAnalyzer(fasta=fasta_file, output=output_file)
-# protan.parse(fasta_file)
-protan.get_stats(fasta_file, output_file)
+
+@click.command()
+@click.argument("filename", type=click.Path(exists=True, dir_okay=False, readable=True, path_type=Path))
+def parse(filename):
+    protan = ProteinAnalyzer(filename)
+    protan.parse()
+
+@click.command()
+@click.argument("filename", type=click.Path(exists=True, dir_okay=False, readable=True, path_type=Path))
+@click.option('-o', '--output', type=click.Path(dir_okay=False, writable=True, path_type=Path))
+# @click.option('-f', '--format', type=click.Choice(['text', 'csv', 'json']), default='text', help='Output format')
+def stats(filename, output):
+    protan = ProteinAnalyzer(filename)
+    if output:
+        with open(output, 'w') as f:
+            f.write(str(protan.get_stats()))
+    else:
+        click.echo(protan.get_stats())
+
+cli.add_command(parse)
+cli.add_command(stats)
+
+if __name__ == '__main__':
+    cli()
--- src/analyzer.py
+++ src/analyzer.py
@@ -11,12 +11,11 @@ negative_polar = ['D' ,'E']
 
 class ProteinAnalyzer:
 
-    def __init__(self, fasta:Path, output:Path):
+    def __init__(self, fasta:Path):
         self.fasta = fasta
-        self.output = output
 
 
-    def parse(self, fasta):
+    def parse(self):
         """
         Parse FASTA from file.fasta
         Args:
@@ -25,34 +24,40 @@ class ProteinAnalyzer:
             SeqRecord
         """
 
-        for seq_record in SeqIO.parse(fasta, "fasta"):
+        for seq_record in SeqIO.parse(self.fasta, "fasta"):
             print(seq_record.id)
             print(repr(seq_record.seq))
             print(len(seq_record))
 
 
-    def get_stats(self, fasta, output):
+    def get_stats(self):
 
         nonpolar_score = 0
         unch_pol_score = 0
         pos_pol_score = 0
         neg_pol_score = 0
 
-        for seq_record in SeqIO.parse(fasta, 'fasta'):
+        for seq_record in SeqIO.parse(self.fasta, 'fasta'):
             aa_score = Counter(seq_record)
             nonpolar_score += sum(aa_score[aa] for aa in nonpolar)
             unch_pol_score += sum(aa_score[aa] for aa in uncharged_polar)
             pos_pol_score += sum(aa_score[aa] for aa in positive_polar)
             neg_pol_score += sum(aa_score[aa] for  aa in negative_polar)
+        return({
+            'Nonpolar AA score': nonpolar_score,
+            'Uncharged polar AA score': unch_pol_score,
+            'Positively polar AA score': pos_pol_score,
+            'Negatively polar AA score': neg_pol_score
+                })
 
-        if output:
-            with open(output, "w") as f:
-                f.write(f"""Nonpolar AA score: {nonpolar_score}
-Uncharged polar AA score: {unch_pol_score}
-Positively polar AA score: {pos_pol_score}
-Negatively polar AA score: {neg_pol_score}""")
-        else:
-            print(f"""Nonpolar AA score: {nonpolar_score}
-Uncharged polar AA score: {unch_pol_score}
-Positively polar AA score: {pos_pol_score}
-Negatively polar AA score: {neg_pol_score}""")
+#         if output:
+#             with open(output, "w") as f:
+#                 f.write(f"""Nonpolar AA score: {nonpolar_score}
+# Uncharged polar AA score: {unch_pol_score}
+# Positively polar AA score: {pos_pol_score}
+# Negatively polar AA score: {neg_pol_score}""")
+#         else:
+#             print(f"""Nonpolar AA score: {nonpolar_score}
+# Uncharged polar AA score: {unch_pol_score}
+# Positively polar AA score: {pos_pol_score}
+# Negatively polar AA score: {neg_pol_score}""")
